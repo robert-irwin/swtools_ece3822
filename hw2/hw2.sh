ce3f5e80f19f73e03bac796a8cdb19825e3cad96
Robert Irwin <robert.irwin@temple.edu>
2015-09-10 16:41:59 -0400

    Updated Description in Comments

--- hw2/hw2.sh
+++ hw2/hw2.sh
@@ -4,7 +4,9 @@
 ##--- Description: Completes HW Assignment 02 for ECE_3822                    
 ##---              This assignment involves writing a script                 
 ##---              that writes a script that renames a large 
-##---              number of files.
+##---              number of files. This script takes one cmd
+##---              line argument. This argument is expected to
+##---              be a list of filenames.
 ##--- Author:      Robert Irwin                          
 
 FILES=$1
